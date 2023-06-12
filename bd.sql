aa2b2b1cb8db98792b32e9c6d0a5f122ee669635
Nozen <ramirezedelvalle@gmail.com>
2023-06-12 15:36:17 -0600

    actualizar

--- bd.sql
+++ bd.sql
@@ -18,12 +18,12 @@ CREATE TABLE areas  (
 
 CREATE TABLE empleados (
     emp_cod SERIAL NOT NULL,
-    emp_nom VARCHAR(200) NOT NULL,
-    emp_ape VARCHAR(200) NOT NULL,
+    emp_nom VARCHAR(100) NOT NULL,
+    emp_ape VARCHAR(100) NOT NULL,
     emp_dpi BIGINT CHECK (emp_dpi >= 1111111111111 AND emp_dpi <= 9999999999999) NOT NULL,
     emp_edad INTEGER CHECK (emp_edad >= 18 AND emp_edad <= 65) NOT NULL,
     emp_puesto_cod INTEGER NOT NULL,
-    emp_sexo VARCHAR (50) NOT NULL,
+    emp_sexo VARCHAR (100) NOT NULL,
     emp_area_cod INTEGER NOT NULL,
     emp_situacion SMALLINT DEFAULT 1,
     PRIMARY KEY(emp_cod),
